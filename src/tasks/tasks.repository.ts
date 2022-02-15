9c580adf9769b34fa61f74b5d97546dd419ad8e3
safoinetl <safoinetouil3@gmail.com>
2022-02-15 14:06:26 +0100

    my first commit

---
+++ src/tasks/tasks.repository.ts
@@ -0,0 +1,41 @@
+import { EntityRepository, Repository } from 'typeorm';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { TaskStatus } from './task-status.enum';
+import { Task } from './task.entity';
+
+@EntityRepository(Task)
+export class TasksRepository extends Repository<Task> {
+  async getTasks(filterDto: GetTasksFilterDto): Promise<Task[]> {
+    const { status, search } = filterDto;
+
+    const query = this.createQueryBuilder('task');
+
+    if (status) {
+      query.andWhere('task.status = :status', { status });
+    }
+
+    if (search) {
+      query.andWhere(
+        'LOWER(task.title) LIKE LOWER(:search) OR LOWER(task.description) LIKE LOWER(:search)'// for the sensitive input,
+      { search: `%${search}%` /*% makes us to make the search caracter par caracter thats mean no need to entre the whole word to search for it */},
+      );
+    }
+
+    const tasks = await query.getMany();
+    return tasks;
+  }
+
+  async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
+    const { title, description } = createTaskDto;
+
+    const task = this.create({
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    });
+
+    await this.save(task);
+    return task;
+  }
+}
